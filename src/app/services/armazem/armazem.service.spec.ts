2b0fc0f0dd75bf501caa1fb9452a2e23794996a6
Luís Carlos <1190818@isep.ipp.pt>
2022-12-14 17:44:33 +0000

    Testes relativos a criação de armazém
    LEI-108 #comment implementação do teste de criação de armazém

--- src/app/services/armazem/armazem.service.spec.ts
+++ src/app/services/armazem/armazem.service.spec.ts
@@ -3,23 +3,65 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { ArmazemService } from './armazem.service';
+import { Armazem } from 'src/app/dto/armazem';
 
 describe('ArmazemService', () => {
   let service: ArmazemService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers:[
+      providers: [
         ArmazemService
         //HttpErrorHandler
       ]
     });
-    TestBed.configureTestingModule({});
     service = TestBed.inject(ArmazemService);
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  describe('#criarArmazem', () => {
+    it('deve criar um Armazém e retorná-lo', () => {
+
+      const a: Armazem = {
+        identificador: "A10",
+        designacao: "Armazém 10",
+        codigoPostal: "2100-100",
+        numeroPorta: 100,
+        nomeRua: "Rua do Porto",
+        localidade: "Porto",
+        pais: "Portugal",
+        municipio: "Paranhos",
+        latitude: 20.10,
+        longitude: 20.10
+      };
+
+      service.criarArmazem(a.identificador, a.designacao, a.codigoPostal,
+        a.numeroPorta, a.nomeRua, a.localidade, a.pais, a.municipio,
+        a.latitude, a.longitude).subscribe(data => expect(data)
+          .toEqual(a, 'deve retornar o Armazém'), fail);
+
+
+      // pathService should have made one request to POST path
+      const req = httpTestingController.expectOne(service.url);
+      expect(req.request.method).toEqual('POST');
+      expect(req.request.body).toEqual(a);
+
+      // Expect server to return the path after POST
+      const expectedResponse = new HttpResponse(
+        { status: 200, statusText: 'OK', body: a });
+      req.event(expectedResponse);
+    })
+  })
 });
--- src/app/services/armazem/armazem.service.ts
+++ src/app/services/armazem/armazem.service.ts
@@ -1,24 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,  HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Armazem } from 'src/app/dto/armazem';
-import {catchError, map, Observable, throwError} from 'rxjs';
+import { catchError, map, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArmazemService {
 
-  private url = 'https://localhost:5001/api/armazem';
+  public url = 'https://localhost:5001/api/armazem';
   constructor(private httpClient: HttpClient) { }
 
 
   criarArmazem(identificador: string, designacao: string, codigoPostal: string, numeroPorta: number, nomeRua: string, localidade: string,
-    pais: string, municipio: string, latitude: number,longitude: number): Observable<any>{
+    pais: string, municipio: string, latitude: number, longitude: number): Observable<any> {
 
-    const body = {"Identificador":identificador, "Designação":designacao, "CódigoPostal":codigoPostal, "NúmeroPorta":numeroPorta,
-    "NomeRua":nomeRua, "Localidade":localidade, "País":pais, "Munícipio":municipio, "Latitude":latitude, "Longitude": longitude}
+    const body = {
+      "identificador": identificador, "designacao": designacao, "codigoPostal": codigoPostal, "numeroPorta": numeroPorta,
+      "nomeRua": nomeRua, "localidade": localidade, "pais": pais, "municipio": municipio, "latitude": latitude, "longitude": longitude
+    }
     console.log(body);
-      
+
     return this.httpClient.post(this.url, body).pipe(catchError(err => {
       if (err.status == 400) {
         alert('Armazém não criado!');
@@ -28,7 +30,7 @@ export class ArmazemService {
       }
       return throwError(err);
     }));
-  
+
   }
 
   getArmazens(): Observable<any> {
@@ -48,7 +50,7 @@ export class ArmazemService {
         let td_designacao = tr.insertCell();
         let td_codigoPostal = tr.insertCell();
         let td_numeroPorta = tr.insertCell();
-        let td_nomeRua= tr.insertCell();
+        let td_nomeRua = tr.insertCell();
         let td_localidade = tr.insertCell();
         let td_pais = tr.insertCell();
         let td_municipio = tr.insertCell();
@@ -69,7 +71,7 @@ export class ArmazemService {
     });
   }
 
-  public extractData(res:any){
+  public extractData(res: any) {
     return res || {};
   }
 }
